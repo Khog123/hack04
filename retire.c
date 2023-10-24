0ce3b4730efb9b4678bcfd9b85f3af57dd472e85
Asif_Nawaz <khogkhan949@gmail.com>
2023-10-24 18:13:21 -0400

    retire.P Initialized

---
+++ retire.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+//the programe is about to plan the retirement saving
+int main(){
+    
+    double startBalance, monthlyContribuion, annualReturnRate, annualInflationRate, retirementYear;
+    double balance, interest,month,justInflationReturnRate, totalInflationInterestEarned;
+    
+    //Take infomation from the user
+    printf("Enter your initial starting balance");
+    scanf("%lf",&startBalance);
+    printf("A monthly contribution Amout(We well)")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
