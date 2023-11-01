1b6a9d0ed0ca317fdd5ac12634f6ae427acc3660
Asif_Nawaz <khogkhan949@gmail.com>
2023-11-01 15:22:59 -0400

    Statements edde

--- retire.c
+++ retire.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
+
 
 //the programe is about to plan the retirement saving
 int main(){
     
-    double startBalance, monthlyContribuion, annualReturnRate, annualInflationRate, retirementYear;
-    double balance, interest,month,justInflationReturnRate, totalInflationInterestEarned;
+    double startBalance, monthlyContributionAmount, annualReturnRate, annualInflationRate, retirementYear;
+    double balance, interest,month,adjustInflationReturnRate, totalInterestEarned;
     
     //Take infomation from the user
     printf("Enter your initial starting balance");
     scanf("%lf",&startBalance);
-    printf("A monthly contribution Amout(We well asume it will be the same over the life of the saving plan):");
+    printf("A monthly contribution Amount(We'll asume it will be the same over the life of the saving plan):");
     scanf("%lf",&monthlyContributionAmount);
     printf("An(average) anual rate of return(on the scale(0,1)):");
-    scanf("%lf",&annnualReturnRate);
+    scanf("%lf",&annualReturnRate);
     printf("An(average) annual rate of inflation(on the scale(0,1)):");
-    scanf("%lf",&annualInlationRate);
+    scanf("%lf",&annualInflationRate);
     printf("Enter a number of year until retirement:");
-    scanf("%lf",&retirement);
+    scanf("%lf",&retirementYear     );
 
-    //years convert to month and programe according the month base and also total 
+    //years converting to months  
     month = retirementYear*12;
-    adjustInflationReturnRate=((1+annualReturnRate)/(1+annualInflationRate=))-1;
+    adjustInflationReturnRate = (1 + annualReturnRate ) / ( 1 + annualInflationRate);
     printf("|Month|\t|Interest   |\tBalance            |\n");
     for (int i=1; i<=month; i++){
         interest=(adjustInflationReturnRate*startBalance)/12;
@@ -36,7 +36,7 @@ int main(){
     }
     printf("______________________________________________");
     printf("Total interest Earned: $ %0.2lf\n",totalInterestEarned);
-    printf("Total nest egg: %0.2lf\n",startBalance);
+    printf("Total Nest Egg: %0.2lf\n",startBalance);
 
     return 0;
 }
---
+++ soundPressureLevel.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    double decibel, level;
+
+    printf("Enter the decibel level: ");
+    scanf("%lf", &decibel_lvel);
+
+    if (decibel_level >= 0 && decibel_level <= 60){
+        printf("Quiet");  
+} else if (decibel_level >= 61 && decibel_level <= 70){
+    printf("Conversational");
+}else if (decibel_level >= 71 && decibel_level <= 90){
+    printf("Loud\n");
+}else if (decibel_level >= 91 && decibel_level <= 110){
+    printf("Very Loud\n");
+}else if (decibel_level >= 111 && decibel_level <= 129){
+    printf("Dangerous\n");
+}else if (decibel_level >= 130 && decibel_level <= 194){
+    printf("Very Dangerous\n");
+}
+}
